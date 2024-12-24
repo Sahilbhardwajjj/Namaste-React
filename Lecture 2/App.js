d726633efa9ba829f204d44dd52f8b23808b4178
SAHIL BHARDWAJ <krishnabhardwaj200517@gmail.com>
2024-12-24 12:56:22 +0530

    Add files via upload
    Lecture 2

---
+++ Lecture 2/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading1" }, "Hello from h1 tag"),
+    React.createElement("h2", { id: "heading2" }, "Hello from h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading1" }, "Hello from h1 tag"),
+    React.createElement("h2", { id: "heading2" }, "Hello from h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//object(React element) is changed to all the elements tag by render fn and then put it into root(div tag)
+//render tag replaces not appends
+root.render(heading);
